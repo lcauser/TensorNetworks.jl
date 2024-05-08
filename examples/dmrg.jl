e776a3db30cf260120311aec1269ad6b1d810361
Luke Causer <luke.causer@outlook.com>
2024-05-08 14:38:18 +0100

    Revert "Added a full working version of DMRG with an end-to-end test"
    This reverts commit a5d6d9ce25fce3ae9737499890d08db840a92a3a.

--- examples/dmrg.jl
+++ examples/dmrg.jl
@@ -3,25 +3,24 @@
     DMRG is a method to target area-law ground states for gapped 1D systems, although 
     there are numerous extensions for excited states, 2D systems and time-evolution.
 =#
-include("../src/TensorNetworks.jl")
-using .TensorNetworks
+using TensorNetworks
 
 ### Model parameters
-N = 40 # System size 
+N = 100 # System size 
 h = 1.0 # Transverse field 
-g = 0.00 # Longitudinal field 
-J = 0.9 # Coupling
+g = 0.05 # Longitudinal field 
+J = 1.2 # Coupling
 
 
 ### Create the hamiltonian as an MPO 
 sh = spinhalf() # Tells the algorithms that we are using a spin half systems
 H = OpList(N) # Creates a list of operators contained in the hamiltonian
 for i = 1:N 
-    add!(H, "x", i, -h) # Add transverse field
-    add!(H, "z", i, -g) # Add a magnetic field
+    add!(H, "x", i, h) # Add transverse field
+    add!(H, "z", i, g) # Add a magnetic field
 end
 for i = 1:N-1
-    add!(H, ["z", "z"], [i, i+1], -J) # Adds coupling
+    add!(H, ["z", "z"], [i, i+1], J) # Adds coupling
 end
 H = MPO(sh, H) # Convert to MPO
 
@@ -35,7 +34,25 @@ psi = randomMPS(2, N, 1) # 2 is the physical dimension, 1 is the bond dimension
 ### Perform DMRG
 # nsites (default 2) is the number of tensors in each update; nsites=1 is single site vMPS, nsites=2 is DMRG 
 # cutoff is the acceptable SVD, maxdim is the maximum bond dimension
-psi, energy = dmrg(psi, H; nsites=2, cutoff=1e-12, maxdim=128, maxsweeps=20, minsweeps=20);
+psi, energy = dmrg(psi, H; nsites=2, cutoff=1e-12, maxdim=32, maxsweeps=100)
 
-psi2 = randomMPS(2, N, 1) # 2 is the physical dimension, 1 is the bond dimension
-psi2, energy2 = dmrg(psi, H, 10*psi; nsites=2, cutoff=1e-12, maxdim=128, maxsweeps=100)
+### Measure observables
+# Measure magnetizations
+oplist = OpList(N)
+for i = 1:N
+    add!(oplist, "z", i)
+end
+for i = 1:N
+    add!(oplist, "x", i)
+end
+
+# Measure couplings
+for i = 1:N-1
+    add!(oplist, ["z", "z"], [i, i+1])
+end
+
+# Take measurements
+expectations = inner(sh, psi, oplist, psi)
+Zs = expectations[1:N]
+Xs = expectations[N+1:2*N]
+ZZs = expectations[2*N+1:end]
